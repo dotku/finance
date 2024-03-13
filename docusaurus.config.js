b5b94a9c29ed9bb0a26cbac94a5e8c161decf72a
Weijing Jay Lin <weijingjaylin@gmail.com>
2024-03-13 02:06:33 -0700

    use docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,7 +76,7 @@ const config = {
             type: "docSidebar",
             sidebarId: "tutorialSidebar",
             position: "left",
-            label: "Tutorial",
+            label: "Docs",
           },
           { to: "/blog", label: "Blog", position: "left" },
 
@@ -94,7 +94,7 @@ const config = {
             title: "Docs",
             items: [
               {
-                label: "Tutorial",
+                label: "Docs",
                 to: "/docs/intro",
               },
             ],
---
+++ src/components/HomepageFeatures/HomepageFeatures.js
@@ -0,0 +1,79 @@
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
+
+const FeatureList = [
+  {
+    title: "Practical Tips",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
+    description: (
+      <>
+        DinoFinance was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
+      </>
+    ),
+  },
+  {
+    title: "Focus on What Matters",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
+    description: (
+      <>
+        DinoFinance lets you focus on your finance goal, and we&apos;ll do the
+        chores. Go ahead and checkout the <code>docs</code>.
+      </>
+    ),
+  },
+  {
+    title: "Calculator",
+    url: "https://dotku.us/finance/calculator/",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
+    description: (
+      <>
+        See how much you can reach by using compound interest calculator with
+        annual fixed deposit
+      </>
+    ),
+  },
+];
+
+// {
+//   label: "Calculator",
+//   to: "pathname:///calculator/",
+// },
+
+function Feature({ Svg, title, description, url }) {
+  return (
+    <div className={clsx("col col--4")}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">
+          {url ? (
+            <Link to={url} isNavLink={true}>
+              {title}
+            </Link>
+          ) : (
+            title
+          )}
+        </Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
